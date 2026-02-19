469998cf71ded4f40e54e87f6a8e55929e878471
Miguel Gonzalez <miguel.gonzalezw8@gmail.com>
2026-02-19 17:28:51 -0600

    chore: stable VolteraHQ desktop build with branding assets before theme branch

--- electron/main.cjs
+++ electron/main.cjs
@@ -3,11 +3,40 @@ const path = require("path");
 
 const isDev = !app.isPackaged;
 
+let splashWin = null;
+
+function createSplash() {
+  splashWin = new BrowserWindow({
+    width: 520,
+    height: 320,
+    frame: false,
+    resizable: false,
+    movable: true,
+    alwaysOnTop: true,
+    center: true,
+    show: false,
+    backgroundColor: "#0b1220",
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  splashWin.loadFile(path.join(__dirname, "splash.html"));
+
+  splashWin.once("ready-to-show", () => {
+    if (splashWin) splashWin.show();
+  });
+}
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1400,
     height: 900,
     autoHideMenuBar: true,
+    show: false, // ✅ don’t show until ready, so splash is seen first
+    backgroundColor: "#0b1220",
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: false
@@ -16,23 +45,30 @@ function createWindow() {
 
   if (isDev) {
     win.loadURL("http://localhost:5173");
-    if (isDev) {
-  win.loadURL("http://localhost:5173");
-  // win.webContents.openDevTools({ mode: "detach" }); // <- keep commented unless you want it
-} else {
-  win.loadFile(path.join(__dirname, "../client/dist/index.html"));
-}
-
+    // win.webContents.openDevTools({ mode: "detach" }); // <- keep commented unless you want it
   } else {
     win.loadFile(path.join(__dirname, "../client/dist/index.html"));
   }
+
+  win.once("ready-to-show", () => {
+    win.show();
+
+    if (splashWin) {
+      splashWin.close();
+      splashWin = null;
+    }
+  });
 }
 
 app.whenReady().then(() => {
+  createSplash();
   createWindow();
 
   app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createSplash();
+      createWindow();
+    }
   });
 });
 
